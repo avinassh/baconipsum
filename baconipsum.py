2635b665c1b66b7cf03612a281de7c177cf901b1
David Xia <david@davidxia.com>
2012-05-17 19:46:41 -0400

    Removed unused db stuff

--- baconipsum.py
+++ baconipsum.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import json
 from sqlite3 import dbapi2 as sqlite3
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
@@ -21,32 +20,6 @@ if app.config[ "DEBUG" ]:
     app.wsgi_app = SharedDataMiddleware( app.wsgi_app, {
         "/": os.path.join( os.path.dirname( __file__ ), 'static' )
     })
-
-
-def connect_db():
-    """Returns a new connection to the database."""
-    return sqlite3.connect( app.config[ "DATABASE" ] )
-
-
-def init_db():
-    """Creates the database tables."""
-    with closing( connect_db() ) as db:
-        with app.open_resource( "schema.sql" ) as f:
-            db.cursor().executescript( f.read() )
-        db.commit()
-
-
-@app.before_request
-def before_request():
-    """Make sure we are connected to the database each request."""
-    g.db = connect_db()
-
-
-@app.teardown_request
-def teardown_request( exception ):
-    """Closes the database again at the end of the request."""
-    if hasattr( g, "db" ):
-        g.db.close()
 
 
 @app.route( "/" )
